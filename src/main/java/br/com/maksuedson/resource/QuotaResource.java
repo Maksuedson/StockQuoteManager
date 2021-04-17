449f88e3755e6be78179345f2ade5673be75d4be
Maksuedson <maksuedson@hotmail.com>
2021-04-17 17:13:07 -0300

    Post request added

--- src/main/java/br/com/maksuedson/resource/QuotaResource.java
+++ src/main/java/br/com/maksuedson/resource/QuotaResource.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +34,11 @@ public class QuotaResource {
 	@DeleteMapping ( path = ("{id}"))
 	public void deleteById(@PathVariable ("id") String id) {
 		repo.deleteById(id);
+	}
+	
+	@PostMapping
+	public Quota addQuota(@RequestBody Quota obj) {
+		return repo.save(obj);
 	}
 
 }
