dd63c5ddf81233bcec80916b35cf9eed72a8c916
Maksuedson <maksuedson@hotmail.com>
2021-04-17 17:09:51 -0300

    Resource added

---
+++ src/main/java/br/com/maksuedson/resource/QuotaResource.java
@@ -0,0 +1,37 @@
+package br.com.maksuedson.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.maksuedson.entity.Quota;
+import br.com.maksuedson.repository.QuotaRepository;
+
+@RestController
+@RequestMapping (value = "/quotas")
+public class QuotaResource {
+	
+	@Autowired
+	QuotaRepository repo;
+	
+	@GetMapping
+	public List<Quota> findAll(){		
+		return repo.findAll();
+	}
+	
+	@GetMapping ( path = ("{id}"))
+	public Quota findQuotaById(@PathVariable ("id") String id) {		
+		return repo.findById(id).orElse(null);		
+	}
+	
+	@DeleteMapping ( path = ("{id}"))
+	public void deleteById(@PathVariable ("id") String id) {
+		repo.deleteById(id);
+	}
+
+}
